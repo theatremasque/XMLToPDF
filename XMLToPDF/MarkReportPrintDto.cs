15082e65d11044d89f3e032b724dac0fd5407917
Oleksander <alexanderkostrytsia@gmail.com>
2023-04-24 14:42:36 +0300

    Add project files.

---
+++ XMLToPDF/MarkReportPrintDto.cs
@@ -0,0 +1,63 @@
+﻿namespace XMLToPDF
+{
+    public class MarkDto
+    {
+        public string FullName { get; set; }
+        public int Value { get; set; }
+    }
+
+    public class MarkReportPrintDto
+    {
+        public static MarkReportPrintDto Instanse { get; private set; } = new()
+        {
+            Id = 1,
+            Date = DateTime.Now,
+            StudyProgram = "Менеджмент туризму та гостинності",
+            Specialization = "Економічна кібернетика",
+            Semester = 2,
+            GroupName = "НАТ-221",
+            SubjectComponentTitle = "Математика",
+            EmployeeSubjectName = "Кротяра Михаил Юславич",
+            VeryfiEmployeeSubjectName = "Котяра Юсав Михаилич",
+            UniversityDecan = "Припилько Анастасія Ананасівна",
+            AnnualYear = 2023,
+            Course = 1,
+            StudyPlanId = 1,
+
+            Marks = new List<MarkDto>()
+            {
+                new MarkDto{ FullName = "Губкин Феликс Эмануилович", Value = 100 },
+                new MarkDto{ FullName = "Лупкина Эльвира Зигфридовна", Value = 90 },
+                new MarkDto{ FullName = "Залупин Феликс Эмануилович", Value = 86 },
+                new MarkDto{ FullName = "Комир Эльвира Зигфридовна", Value = 80 },
+                new MarkDto{ FullName = "Вафлея Феликс Эмануилович", Value = 76 },
+                new MarkDto{ FullName = "Фрем Феликс Эмануилович", Value = 66 },
+                new MarkDto{ FullName = "Зонтик Феликс Эмануилович", Value = 59 },
+                new MarkDto{ FullName = "Шибора Эльвира Зигфридовна", Value = 48 },
+                new MarkDto{ FullName = "Головач Лена Зигфридовна", Value = 21 },
+                new MarkDto{ FullName = "Красиво Эльвира Зигфридовна", Value = 10 },
+            }
+        };
+
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+
+        public string Specialization { get; set; }
+
+        public string StudyProgram { get; set; }
+
+        public int Semester { get; set; }
+
+        public string SubjectComponentTitle { get; set; }
+        public string GroupName { get; set; }
+        public string EmployeeSubjectName { get; set; }
+        public string VeryfiEmployeeSubjectName { get; set; }
+        public string UniversityDecan { get; set; }
+
+        public int Course { get; set; }
+
+        public int StudyPlanId { get; set; }
+        public int AnnualYear { get; set; }
+        public ICollection<MarkDto> Marks { get; set; }
+    }
+}
---
+++ XMLToPDF/Program.cs
@@ -0,0 +1,82 @@
+﻿using OfficeOpenXml;
+using Spire.Xls;
+using System.Data;
+using System.Globalization;
+
+namespace XMLToPDF
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            var newReport = new ExcelPackage();
+
+            ExcelWorksheet worksheet;
+
+            using (var report = new ExcelPackage(@"C:\Users\kostr\OneDrive\Робочий стіл\baseTemplate.xlsx"))
+            {
+                worksheet = newReport.Workbook.Worksheets.Add("newTemplate", report.Workbook.Worksheets[0]);
+            }
+
+            var model = MarkReportPrintDto.Instanse;
+
+            // Report block
+
+            //table 1
+            worksheet.Cells["C4"].Value = model.Specialization;
+            worksheet.Cells["C5"].Value = model.StudyProgram;
+            worksheet.Cells["C6"].Value = model.AnnualYear;
+            //table 2
+            worksheet.Cells["H5"].Value = model.Semester;
+            worksheet.Cells["H6"].Value = model.Course;
+            worksheet.Cells["H7"].Value = model.GroupName;
+            //table 3
+
+            // block date
+            worksheet.Cells["D10"].Value = model.Date.ToString("M", CultureInfo.GetCultureInfo("uk-UA"));
+            worksheet.Cells["F10"].Value = model.Date.Year + " року";
+
+
+            worksheet.Cells["E9"].Value = model.Id;
+            worksheet.Cells["C11"].Value = model.SubjectComponentTitle;
+            worksheet.Cells["D13"].Value = model.EmployeeSubjectName;
+            worksheet.Cells["D14"].Value = model.VeryfiEmployeeSubjectName;
+
+            var decan = model.UniversityDecan;
+            worksheet.Cells["A49"].Value = $"Директор інституту: {decan} ____________________";
+
+            var employeeSubject = model.EmployeeSubjectName;
+            worksheet.Cells["A62"].Value = $"Екзаменатор (викладач): {employeeSubject} ____________________";
+
+            //Mark block
+            int cell = 18;
+            worksheet.Cells["D18"].Value = model.StudyPlanId;
+
+            foreach (var item in model.Marks)
+            {
+                worksheet.Cells["D" + cell].Value = model.StudyPlanId;
+                worksheet.Cells["B" + cell].Value = item.FullName;
+                worksheet.Cells["E" + cell].Value = item.Value;
+                ++cell;
+            }
+
+
+            newReport.SaveAs(@"C:\Users\kostr\OneDrive\Робочий стіл\LeshaPussy.xlsx");
+
+            // pdf part
+
+            Workbook convertPdf = new Workbook();
+
+            convertPdf.LoadFromFile(newReport.File.FullName);
+
+            convertPdf.ConverterSetting.SheetFitToPage = true;
+
+            convertPdf.SaveToFile("TestConvertXmlTo3.pdf", FileFormat.PDF);
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
